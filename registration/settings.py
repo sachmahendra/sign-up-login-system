8d1c6e4fc1abe6ee384cbf88174fd4891dfaf818
Mahendra Kumar Yadav <123411058+sachmahendra@users.noreply.github.com>
2024-05-19 16:50:12 +0530

    Update settings.py

--- registration/settings.py
+++ registration/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ao1p&(%@n+o6=xj6sn1slvw-k55z))rmzp4zu+!f(cu856ibvg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
